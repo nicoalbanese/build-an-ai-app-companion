e2096059f30c67602bfc768516a42d647a0d4f44
Nico Albanese <nico.albanese@vercel.com>
2024-05-20 09:38:14 +0100

    calling llm section done

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
-import createMDX from 'fumadocs-mdx/config';
+import createMDX from "fumadocs-mdx/config";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      { source: "/", destination: "/docs/introduction", permanent: true },
+      { source: "/docs", destination: "/docs/introduction", permanent: true },
+    ];
+  },
 };
 
 export default withMDX(config);
