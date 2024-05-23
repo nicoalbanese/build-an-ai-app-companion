f6ec95b08fd2e0af4e1884ca2c00b049084496ab
Nico Albanese <nico.albanese@vercel.com>
2024-05-22 20:56:42 -0400

    tets

--- app/_components/oai-key.tsx
+++ app/_components/oai-key.tsx
@@ -1,9 +1,11 @@
-import { get } from "@vercel/edge-config";
+// import { get } from "@vercel/edge-config";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 
 export const OpenAIKey = async () => {
-  const key = await get<string>("openai_api_key");
-  const showKey = await get<boolean>("show_key");
+  // const key = await get<string>("openai_api_key");
+  // const showKey = await get<boolean>("show_key");
+  const key = "hello"
+  const showKey = true
   return (
     <CodeBlock>
       <Pre className="px-4 text-[13px]">OPENAI_API_KEY={showKey ? key : "sk-*****-*************"}</Pre>
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,9 @@
 import type { MDXComponents } from 'mdx/types';
 import defaultComponents from 'fumadocs-ui/mdx';
-import { OpenAIKey } from './app/_components/oai-key';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
-    OpenAIKey
   };
 }
