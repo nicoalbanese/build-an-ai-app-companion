fcce130c1b71d4e661d73f08d398bad65dd947e8
Nico Albanese <nico.albanese@vercel.com>
2024-05-22 21:01:05 -0400

    another test

--- app/_components/oai-key.tsx
+++ app/_components/oai-key.tsx
@@ -4,6 +4,9 @@ import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 export const OpenAIKey = async () => {
   // const key = await get<string>("openai_api_key");
   // const showKey = await get<boolean>("show_key");
+  const test = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  const data = await test.json()
+  console.log(data)
   const key = "hello"
   const showKey = true
   return (
