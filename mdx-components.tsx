ad4a7d9a135dfe3f33cbe717a77598f9a7c18d0f
Nico Albanese <nico.albanese@vercel.com>
2024-05-22 20:46:14 -0400

    update

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,11 @@
 import type { MDXComponents } from 'mdx/types';
 import defaultComponents from 'fumadocs-ui/mdx';
+import { OpenAIKey } from './app/_components/oai-key';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    OpenAIKey
   };
 }
