cbd99936a09160d5e8b5e6fb835387b1604d3b95
Nico Albanese <nico.albanese@vercel.com>
2024-05-22 21:54:24 -0400

    updates

--- app/_components/oai-key.tsx
+++ app/_components/oai-key.tsx
@@ -2,14 +2,11 @@ import { get } from "@vercel/edge-config";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 
 export async function OpenAIKey () {
-  const key = await get("openai_api_key");
-  const showKey = await get("show_key");
+  const key = await get<string>("openai_api_key");
+  const showKey = await get<boolean>("show_key");
   return (
     <CodeBlock>
-      {/* <Pre className="px-4 text-[13px]">OPENAI_API_KEY={showKey ? key : "sk-*****-*************"}</Pre> */}
-      <Pre className="px-4 text-[13px]">
-        <pre>{JSON.stringify({ showKey }, null, 2)}</pre>
-      </Pre>
+      <Pre className="px-4 text-[13px]">OPENAI_API_KEY={showKey ? key : "sk-*****-*************"}</Pre>
     </CodeBlock>
   );
 };
