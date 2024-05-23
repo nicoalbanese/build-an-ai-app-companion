c274dad0e905e5d0719f3c6899a5ab050ac035b9
Nico Albanese <nico.albanese@vercel.com>
2024-05-22 21:23:19 -0400

    test

--- app/_components/oai-key.tsx
+++ app/_components/oai-key.tsx
@@ -1,17 +1,15 @@
-// import { get } from "@vercel/edge-config";
+import { get } from "@vercel/edge-config";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 
 export const OpenAIKey = async () => {
-  // const key = await get<string>("openai_api_key");
-  // const showKey = await get<boolean>("show_key");
-  const test = await fetch("https://jsonplaceholder.typicode.com/posts/1")
-  const data = await test.json()
-  console.log(data)
-  const key = "hello"
-  const showKey = true
+  const key = await get("openai_api_key");
+  const showKey = await get("show_key");
   return (
     <CodeBlock>
-      <Pre className="px-4 text-[13px]">OPENAI_API_KEY={showKey ? key : "sk-*****-*************"}</Pre>
+      {/* <Pre className="px-4 text-[13px]">OPENAI_API_KEY={showKey ? key : "sk-*****-*************"}</Pre> */}
+      <Pre className="px-4 text-[13px]">
+        <pre>{JSON.stringify({ showKey }, null, 2)}</pre>
+      </Pre>
     </CodeBlock>
   );
 };
