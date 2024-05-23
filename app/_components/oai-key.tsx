5c1fa87c9d9d27ce85a518d22e9cece2398f237b
Nico Albanese <nico.albanese@vercel.com>
2024-05-23 06:50:16 -0400

    update

--- app/_components/oai-key.tsx
+++ app/_components/oai-key.tsx
@@ -1,8 +1,10 @@
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import { headers } from "next/headers";
 
 export async function OpenAIKey () {
+  const head = headers();
   const res = await fetch(process.env.EC_FETCH ?? "")
-  const {showKey, key} = await res.json()
+  const {openai_api_key: key, show_key: showKey} = await res.json()
   return (
     <CodeBlock>
       <Pre className="px-4 text-[13px]">OPENAI_API_KEY={showKey ? key : "sk-*****-*************"}</Pre>
