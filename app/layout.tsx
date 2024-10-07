78f3c81bc778a891e08a30ce047c8acac2c206ea
Nico Albanese <nico.albanese@vercel.com>
2024-10-07 23:21:51 +0100

    add toolbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
-
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import { Suspense, type ReactNode } from "react";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { VercelToolbar } from "@vercel/toolbar/next";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -15,6 +15,9 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
         <RootProvider>{children}</RootProvider>
+        <Suspense>
+          <VercelToolbar />
+        </Suspense>
       </body>
     </html>
   );
